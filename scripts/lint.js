35ffad98c446634dfc441a63554d3ae12d2e9648
Juan Cruz Viotti <jviotti@openmailbox.org>
2017-02-10 14:29:34 -0400

    Enforce architecture schema
    Signed-off-by: Juan Cruz Viotti <jviotti@openmailbox.org>

--- scripts/lint.js
+++ scripts/lint.js
@@ -2,7 +2,25 @@ const _ = require('lodash');
 const fs = require('fs');
 const path = require('path');
 const recursiveReadSync = require('recursive-readdir-sync');
+const jsonschema = require('jsonschema');
 const ROOT_PATH = path.join(__dirname, '..');
+
+const SCHEMAS = _.chain(fs.readdirSync(path.join(ROOT_PATH, 'schemas')))
+  .map((schemaName) => {
+    return JSON.parse(fs.readFileSync(path.join(ROOT_PATH, 'schemas', schemaName), {
+      encoding: 'utf8'
+    }));
+  })
+  .map((schema) => {
+    return [ schema.title, schema ];
+  })
+  .fromPairs()
+  .value();
+
+const validate = (contract, schema) => {
+  const validator = new jsonschema.Validator();
+  return validator.validate(contract, schema).errors;
+};
 
 const CONTRACTS = _.chain(recursiveReadSync(ROOT_PATH))
   .map(_.partial(path.relative, ROOT_PATH))
@@ -67,5 +85,25 @@ _.each(CONTRACTS, (contract) => {
         console.error(`\t\t${type} -> ${name}`);
       }
     });
+  }
+
+  if (contract.contents.type === 'architecture') {
+    const schema = _.get(SCHEMAS, contract.contents.type);
+
+    if (_.isUndefined(schema)) {
+      console.error('Contracts should have an accompanying schema');
+      console.error(`\t${contract.contents.type}`);
+    }
+
+    const validationErrors = validate(contract.contents, schema);
+
+    if (!_.isEmpty(validationErrors)) {
+      console.error('Contracts should adhere to their schema');
+      console.error(`\t${contract.name}`);
+
+      _.each(validationErrors, (error) => {
+        console.error(`\t\t${error.stack}`);
+      });
+    }
   }
 });
